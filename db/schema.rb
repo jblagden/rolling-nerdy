a3536ed6121ca31d4c813c5ba5f745e015f18563
Justin Blagden <jbemails2@gmail.com>
2016-11-16 20:36:58 -0600

    Added Type table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117022556) do
+ActiveRecord::Schema.define(version: 20161117022643) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -67,6 +67,12 @@ ActiveRecord::Schema.define(version: 20161117022556) do
   create_table "provinces", force: :cascade do |t|
     t.string   "name"
     t.integer  "tax"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
