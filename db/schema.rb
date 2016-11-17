02c3cdcd834c22a76173edfaad36229ec8a45535
Justin Blagden <jbemails2@gmail.com>
2016-11-16 20:46:09 -0600

    Added status table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117024036) do
+ActiveRecord::Schema.define(version: 20161117024141) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -74,6 +74,12 @@ ActiveRecord::Schema.define(version: 20161117024036) do
   create_table "provinces", force: :cascade do |t|
     t.string   "name"
     t.integer  "tax"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
