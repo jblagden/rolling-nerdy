38e71e22c4cf648196ba4338d254afd10cda2f8d
Justin Blagden <jbemails2@gmail.com>
2016-11-16 20:26:19 -0600

    Added colour table

---
+++ app/models/colour.rb
@@ -0,0 +1,2 @@
+class Colour < ApplicationRecord
+end
---
+++ db/migrate/20161117022556_create_colours.rb
@@ -0,0 +1,9 @@
+class CreateColours < ActiveRecord::Migration[5.0]
+  def change
+    create_table :colours do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
