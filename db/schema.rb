1554348079958f7a937ece3def212092c75e8ad5
Justin Blagden <jbemails2@gmail.com>
2016-11-16 20:41:05 -0600

    Added Country table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117022643) do
+ActiveRecord::Schema.define(version: 20161117024036) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20161117022643) do
 
   create_table "colours", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "tax_rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
