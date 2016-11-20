6fc71fa83c86e52398e52b71b5c085acdec6eb75
Justin Blagden <jbemails2@gmail.com>
2016-11-19 22:52:09 -0600

    Updates ammend's model comments

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  material_id :integer
+#  type_id     :integer
+#  colour_id   :integer
+#  price       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image       :string
+#
+
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
