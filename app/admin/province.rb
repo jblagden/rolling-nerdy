fcf632f3faf8193fbc643976f2d42ae0af9ccff8
Justin Blagden <jbemails2@gmail.com>
2016-11-21 18:11:16 -0600

    Add usehst to active admin permit params

--- app/admin/province.rb
+++ app/admin/province.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Province do
 
-  permit_params :name, :tax_rate, :country_id
+  permit_params :name, :tax_rate, :country_id, :useshst
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
