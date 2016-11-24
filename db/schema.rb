bf3ac09e92caba98d75a70c670157560e01de05f
Justin Blagden <jbemails2@gmail.com>
2016-11-23 18:50:29 -0600

    Add price to product as decimal(12,3)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124004631) do
+ActiveRecord::Schema.define(version: 20161124004830) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -102,9 +102,10 @@ ActiveRecord::Schema.define(version: 20161124004631) do
     t.integer  "material_id"
     t.integer  "type_id"
     t.integer  "colour_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "image"
+    t.decimal  "price",       precision: 12, scale: 3
     t.index ["colour_id"], name: "index_products_on_colour_id"
     t.index ["material_id"], name: "index_products_on_material_id"
     t.index ["type_id"], name: "index_products_on_type_id"
