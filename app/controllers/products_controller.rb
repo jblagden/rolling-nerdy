c8e5aca66f9c9d555b961d87bc4fa65e1ced7fb9
Justin Blagden <jbemails2@gmail.com>
2016-11-23 22:29:36 -0600

    Create Products, Carts and Line Items controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,4 @@
-# == Schema Information
-#
-# Table name: products
-#
-#  id          :integer          not null, primary key
-#  name        :string
-#  material_id :integer
-#  type_id     :integer
-#  colour_id   :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  image       :string
-#  price       :decimal(12, 3)
-#
-
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
   end
 end
