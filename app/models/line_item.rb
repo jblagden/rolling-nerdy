70a2c431400a7bb4841cda3b8f9e2264cd0c6c6f
Justin Blagden <jbemails2@gmail.com>
2016-11-25 02:31:12 -0600

    Fix Style/SpaceInsideHashLiteralBraces

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -16,7 +16,7 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  validates :quantity, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   validate :product_present
   validate :order_present
