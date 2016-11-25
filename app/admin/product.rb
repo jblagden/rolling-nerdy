4ec0989ab80bd7afebdfd1a40a22019a661dc1ca
Justin Blagden <jbemails2@gmail.com>
2016-11-25 02:20:28 -0600

    Fix Style/ExtraSpacing

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Product do
 
-  permit_params :name, :material_id, :type_id, :colour_id, :price,  :image
+  permit_params :name, :material_id, :type_id, :colour_id, :price, :image
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
